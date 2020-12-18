5f5903ac37cb58acf65c03eaa4ba70b395ffac19
Erie Dias <eriedias7@gmail.com>
2020-12-18 15:36:23 -0300

    Chapter #3 ends

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers:</strong> {{ followers }}
+    <button @click="followUser">
+      Follow
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'eriedias',
+        firstName: 'Erie',
+        lastName: 'Dias',
+        email: 'eriedias7@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount){
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser();
+  },
 }
 </script>
 
@@ -19,8 +52,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
